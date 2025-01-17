6d09c4f19b816c52147861789250030949a81d95
Thales Bitencourt <thalesbitencourt@gmail.com>
2025-01-17 16:09:58 -0300

    chore(#2): added php quality tools.

---
+++ tests/Unit/Entity/UserTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Unit\Entity;
+
+use PHPUnit\Framework\TestCase;
+
+final class UserTest extends TestCase
+{
+    public function testSomething(): void
+    {
+        $this->assertTrue(true);
+    }
+}
