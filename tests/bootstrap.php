f3b218b4fb09f7fd57147f0bcfcbdb7aaadaec4e
Thales Bitencourt <thalesbitencourt@gmail.com>
2025-01-20 08:06:52 -0300

    chore(#4): Fix issue on phpunit tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,24 @@ declare(strict_types=1);
 
 use Symfony\Component\Dotenv\Dotenv;
 
+$resetDatabase = filter_var($_ENV['BOOTSTRAP_RESET_DATABASE'], FILTER_VALIDATE_BOOLEAN, FILTER_NULL_ON_FAILURE);
+if (true === $resetDatabase) {
+    echo 'Resetting test database...';
+    passthru(sprintf(
+        'php "%s/../bin/console" doctrine:schema:drop --env=test --force --no-interaction',
+        __DIR__
+    ));
+    passthru(sprintf(
+        'php "%s/../bin/console" doctrine:schema:update --env=test --force --no-interaction',
+        __DIR__
+    ));
+    passthru(sprintf(
+        'php "%s/../bin/console" doctrine:fixtures:load --env=test --no-interaction',
+        __DIR__
+    ));
+    echo ' Done'.PHP_EOL.PHP_EOL;
+}
+
 require dirname(__DIR__).'/vendor/autoload.php';
 
 if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
@@ -11,17 +29,3 @@ if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
-
-// executes the "php bin/console --env=test doctrine:schema:update" command
-passthru(sprintf(
-    'APP_ENV=%s php "%s/../bin/console" doctrine:schema:update',
-    $_ENV['APP_ENV'],
-    __DIR__
-));
-
-// executes the "php bin/console --env=test doctrine:fixtures:load" command
-// passthru(sprintf(
-//     'APP_ENV=%s php "%s/../bin/console" doctrine:fixtures:load',
-//     $_ENV['APP_ENV'],
-//     __DIR__
-// ));
